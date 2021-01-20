ce864011fe4ab766a1006f2d6c8b0983525eef4d
unknown <chris_amantino97@hotmail.com>
2021-01-20 15:38:00 -0300

    Site pronto para detalhes finais

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '650px'
+}
+
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
